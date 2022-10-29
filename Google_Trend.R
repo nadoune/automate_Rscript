5f99244a4dd5ab4546cccdd4b25c0f6bab9f5de5
nadoune <nada.habli@canada.ca>
2022-10-28 23:48:49 -0400

    Update Google_Trend.R

--- Google_Trend.R
+++ Google_Trend.R
@@ -1,10 +1,20 @@
 library(gtrendsR)
 
-x<- gtrendsR::gtrends("Recession", onlyInterest = TRUE, geo = "CA-ON")
+recession <- gtrendsR::gtrends("Recession", onlyInterest = TRUE, geo = "CA-ON")
+recession1 <-data.frame(recession$interest_over_time)
 
-data1 <-data.frame(x$interest_over_time)
 
-write.csv(data1, file = paste0("data/Recession_trends_", make.names(Sys.time()), ".csv"),row.names = F)
+inflation <- gtrendsR::gtrends("Inflation", onlyInterest = TRUE, geo = "CA-ON")
+inflation1 <-data.frame(inflation$interest_over_time)
+
+
+supplyChain <- gtrendsR::gtrends("Supply chain", onlyInterest = TRUE, geo = "CA-ON")
+supplyChain1 <-data.frame(supplyChain$interest_over_time)
+
+
+write.csv(recession1,   file = paste0("data/Recession_trends_"  , make.names(Sys.time()), ".csv"),row.names = F)
+write.csv(inflation1,   file = paste0("data/Inflation_trends_"  , make.names(Sys.time()), ".csv"),row.names = F)
+write.csv(supplyChain1, file = paste0("data/supplyChain_trends_", make.names(Sys.time()), ".csv"),row.names = F)
 
 #write.csv(data1, file = paste0("data/Recession_trends.csv"), row.names = F)
 
