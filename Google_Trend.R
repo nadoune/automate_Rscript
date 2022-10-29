afe740305c1d72b6837452ba3bef42e8c16b2eb2
nadoune <nada.habli@canada.ca>
2022-10-28 23:01:04 -0400

    Update Google_Trend.R

--- Google_Trend.R
+++ Google_Trend.R
@@ -4,7 +4,7 @@ x<- gtrendsR::gtrends("Recession", onlyInterest = TRUE, geo = "CA-ON")
 
 data1 <-data.frame(x$interest_over_time)
 
-#write.csv(data1, file = paste0("data/Recession_trends_", make.names(Sys.time()), ".csv"),row.names = F)
+write.csv(data1, file = paste0("data/Recession_trends_", make.names(Sys.time()), ".csv"),row.names = F)
 
-write.csv(data1, file = paste0("data/Recession_trends.csv"), row.names = F)
+#write.csv(data1, file = paste0("data/Recession_trends.csv"), row.names = F)
 
